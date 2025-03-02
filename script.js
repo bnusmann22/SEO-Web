26d353a8c0129c233cdecdd5f6f77e16690814ad
bnusmann22 <bn.usmann22@gmail.com>
2025-03-02 14:14:19 +0100

    Initial Commit of The app

---
+++ script.js
@@ -0,0 +1,19 @@
+let userChoice = document.getElementById('changeclr');
+const colorDiv = document.getElementById('red');
+const isValidColor = (color) => {
+  let s = new Option().style;
+  s.color = color;
+  return s.color !== '';
+};
+
+const handleChange = () => {
+  let mainChoice = userChoice.value.trim();
+  mainChoice = mainChoice.toLowerCase();
+  if (!isValidColor(mainChoice)) {
+    console.log('Invalid Color , Please re-Enter a colour');
+  } else {
+    colorDiv.style.backgroundColor = mainChoice;
+    console.log(mainChoice);
+  }
+  userChoice.value = '';
+};
